0bda50f5748731ad4562ee950d37cdb027660fc1
Mainul Islam <mainulislam6168@gmail.com>
2025-07-07 23:04:46 +0600

    if-elif and case statement examples

--- case-statement.sh
+++ case-statement.sh
@@ -1,9 +1,9 @@
 ## Case statement
 case ${1,,} in
- Mainul )
+ u@ser | Mainul )
   echo "You are the boss here, welcome!" ;;
 help )
   echo "Just enter your user name!" ;;
 * )
-  echo "Enter a valid user name!";;
+  echo "Enter a valid user name!"
 esac
