c2c3dc9ccb88277d04df440fcc1d43d4fb54aa8f
Mainul Islam <mainulislam6168@gmail.com>
2025-07-07 22:46:33 +0600

    if-elif and case statement examples

--- 02-variable.sh
+++ 02-variable.sh
@@ -55,12 +55,3 @@ echo added value - $ADD
 substract=$((700-98))
 echo present value - $substract
 ## we can use a-z & A-Z & 0-9 as variable name
-
-## if, elif, else statement
-
-if [ ${1,, } = Mainul ]; then
-  echo "oh, you are my boss here, welcome!"
-  elif [ ${1,, } ]; then
-    echo "Just enter your uName!"
-    else echo "I don't know you!"
-fi
---
+++ case-statement.sh
@@ -0,0 +1,9 @@
+## Case statement
+case ${1,,} in
+username | Mainul )
+  echo "You are the boss here, welcome!" ;;
+help )
+  echo "Just enter your user name!" ;;
+* )
+  echo "Enter a valid user name!";;
+esac
