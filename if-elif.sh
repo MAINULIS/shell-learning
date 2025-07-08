092f9e389c9b04226908bbc5c71152b19a9a053d
Mainul Islam <mainulislam6168@gmail.com>
2025-07-08 08:45:59 +0600

    checking conditions

--- if-elif.sh
+++ if-elif.sh
@@ -16,7 +16,7 @@
 # |Expressions are categorized in 3 types
 # 01 Number comparisons
 # Operator -eq, -ne,-lt, -le, -gt, -ge.
-# [1 -eq 1] ---> true if both is equal
+# [[1 -eq 1]] ---> true if both is equal
 
 
 if [ ${1,, } = Mainul ]; then
@@ -28,6 +28,16 @@ if [ ${1,, } = Mainul ]; then
 fi
 
 x=90
-if [[ $a -lt 100 ]]; then
- echo $a is less than 100
+if [[ $x -lt 100 ]]; then
+ echo $x is less than 100
 fi
+
+# 02 String comparisons
+# String Comparison Operators: =, !=, <, >
+# -z string(True if string is empty (zero length))
+# -n string (True if string is not empty)
+if [[ -z "$a" ]]; then
+    echo a is empty
+fi
+
+### Note: In expression specially in string variable should be inside " ".
